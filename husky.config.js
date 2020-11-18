f03df9b6659bb2d13526a98b0331c821d4e301f8
ofadiman <ofadiman@gmail.com>
2020-11-18 21:00:03 +0100

    feat: add commit lint

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,7 @@ const tasks = (arr) => arr.join(' && ')
 
 module.exports = {
   hooks: {
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': tasks(['yarn check:typescript', 'lint-staged'])
   }
 }
