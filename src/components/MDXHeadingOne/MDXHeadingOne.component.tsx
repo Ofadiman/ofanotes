376451ff05fab954345db56cd9f3b84c687d6f52
ofadiman <ofadiman@gmail.com>
2020-11-29 18:48:17 +0100

    refactor: <MDXHeadingOne /> component
    - move custom types to `/src` directory

--- src/components/MDXHeadingOne/MDXHeadingOne.component.tsx
+++ src/components/MDXHeadingOne/MDXHeadingOne.component.tsx
@@ -1,27 +1,40 @@
+import { useLocation } from '@reach/router'
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 
 import { SEO } from '../SEO/SEO.component'
 import { StyledDate } from './MDXHeadingOne.styles'
-import { MDXHeadingOneProps } from './MDXHeadingOne.types'
+import { MDXHeadingOneProps, MDXHeadingOneQueryResult } from './MDXHeadingOne.types'
 
-export const MDXHeadingOne: FC<MDXHeadingOneProps> = ({ className, children }) => {
-  if (typeof children === 'string') {
-    return (
-      <>
-        <SEO title={children} />
-        <h1 className={className}>{children}</h1>
-      </>
-    )
+export const MDXHeadingOne: FC<MDXHeadingOneProps> = ({ className }) => {
+  const { pathname } = useLocation()
+  const queryResult: MDXHeadingOneQueryResult = useStaticQuery(graphql`
+    query MDXHeadingOneQueryResult {
+      allMdx {
+        edges {
+          node {
+            frontmatter {
+              title
+              createdAt(formatString: "YYYY-MM-DD")
+            }
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  const currentPageData = queryResult.allMdx.edges.find(({ node }) => pathname.includes(node.slug))
+  if (currentPageData === undefined) {
+    throw new Error('"currentPageData" is undefined!')
   }
-
-  const [title, date] = children
 
   return (
     <>
-      <SEO title={title} />
+      <SEO title={currentPageData.node.frontmatter.title} />
       <h1 className={className}>
-        {title}
-        <StyledDate>{date.replace(/]|\[/g, '')}</StyledDate>
+        {currentPageData.node.frontmatter.title}
+        <StyledDate>{`Created at ${currentPageData.node.frontmatter.createdAt}`}</StyledDate>
       </h1>
     </>
   )
--- src/components/MDXHeadingOne/MDXHeadingOne.types.ts
+++ src/components/MDXHeadingOne/MDXHeadingOne.types.ts
@@ -2,3 +2,20 @@ export interface MDXHeadingOneProps {
   children: string[] | string
   className?: string
 }
+
+export interface MDXHeadingOneQueryResult {
+  allMdx: {
+    edges: MDXHeadingOneQueryResultEdge[]
+  }
+}
+
+export interface MDXHeadingOneQueryResultEdge {
+  node: {
+    frontmatter: {
+      createdAt: string
+      tags: string[]
+      title: string
+    }
+    slug: string
+  }
+}
